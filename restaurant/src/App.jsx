60041bf002eeac93dfd06f84e58e9f985ed716a1
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-03-07 16:25:31 +0800

    Refactor container to containers folder

--- restaurant/src/App.jsx
+++ restaurant/src/App.jsx
@@ -1,4 +1,4 @@
-import {AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu} from "./container";
+import {AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu} from "./containers";
 import {Navbar} from "./components";
 import './App.css';
 
